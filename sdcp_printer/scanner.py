44b2726f0077280a0b78b5977f9532133a0f57ed
Kevin Petersen <kevinvkpetersen@gmail.com>
2024-12-18 01:34:39 -0800

    Update scan for multiple printers

--- sdcp_printer/scanner.py
+++ sdcp_printer/scanner.py
@@ -8,16 +8,27 @@ from printer import SDCPPrinter
 logger = logging.getLogger(__name__)
 
 
-def discover_devices():
+def discover_devices(timeout: int = 1) -> list[SDCPPrinter]:
+    '''Broadcasts a discovery message to all devices on the network and waits for responses.'''
+    printers: list[SDCPPrinter] = []
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.settimeout(timeout)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, timeout)
         sock.sendto(b'M99999', ('<broadcast>', BROADCAST_PORT))
 
-        raw_data, address = sock.recvfrom(8192)
-        logger.debug(
-            f'Reply from {address[0]}: {raw_data.decode(MESSAGE_ENCODING)}')
-        printer_json = json.loads(raw_data.decode(MESSAGE_ENCODING))
-        printer_object = SDCPPrinter(printer_json)
-        logger.info(f'Found printer: {printer_object}')
+        logger.info('Starting scan')
+        while True:
+            try:
+                raw_data, address = sock.recvfrom(8192)
+                logger.debug(
+                    f'Reply from {address[0]}: {raw_data.decode(MESSAGE_ENCODING)}')
+                printer_json = json.loads(raw_data.decode(MESSAGE_ENCODING))
+                printers.append(SDCPPrinter(printer_json))
+            except socket.timeout:
+                logger.info('Done scanning')
+                break
+            except json.JSONDecodeError:
+                logger.error(f'Invalid JSON from {address[0]}')
 
-    return printer_object
+    return printers
