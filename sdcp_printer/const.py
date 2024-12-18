f2d5686562c1e58d61ee97d2c7b4a0bea09ccc29
Kevin Petersen <kevinvkpetersen@gmail.com>
2024-12-18 01:16:33 -0800

    Printer object structure

---
+++ sdcp_printer/const.py
@@ -0,0 +1,5 @@
+'''Constants used in the project'''
+
+BROADCAST_PORT = 3000
+
+MESSAGE_ENCODING = 'utf-8'
--- sdcp_printer/scanner.py
+++ sdcp_printer/scanner.py
@@ -1,8 +1,10 @@
-import socket
+import json
 import logging
+import socket
 import sys
 
-BROADCAST_PORT = 3000
+from const import BROADCAST_PORT, MESSAGE_ENCODING
+from printer import Printer
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +15,12 @@ def discover_devices():
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.sendto(b'M99999', ('<broadcast>', BROADCAST_PORT))
 
-        raw_data, address = sock.recvfrom(1024)
-        logger.debug(f'Reply from {address[0]}: {raw_data.decode()}')
+        raw_data, address = sock.recvfrom(8192)
+        logger.debug(
+            f'Reply from {address[0]}: {raw_data.decode(MESSAGE_ENCODING)}')
+        printer_data = json.loads(raw_data.decode(MESSAGE_ENCODING))
+        printer_object = Printer(printer_data)
+        logger.info(f'Found printer: {printer_object}')
 
 
 if __name__ == '__main__':
