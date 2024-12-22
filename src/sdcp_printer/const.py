9deb455ab2b433e618b2e6288773791575a311e6
Kevin Petersen <kevinvkpetersen@gmail.com>
2024-12-22 03:56:27 -0800

    Set up persistent connection

--- src/sdcp_printer/const.py
+++ src/sdcp_printer/const.py
@@ -10,8 +10,9 @@ MESSAGE_ENCODING = 'utf-8'
 
 class SDCPCommand(Enum):
     '''Values for the Cmd field.'''
-    STATUS = 0,
+    STATUS = 0
+
 
 class SDCPFrom(Enum):
     '''Values for the From field.'''
-    PC = 0,
+    PC = 0
--- src/sdcp_printer/printer.py
+++ src/sdcp_printer/printer.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import json
 import logging
+import threading
+import time
 import websocket
 
 from contextlib import closing
@@ -18,6 +20,10 @@ class SDCPPrinter:
         self._id: str = discovery_json['Id']
         self._ip_address: str = discovery_json['Data']['MainboardIP']
         self._mainboard_id: str = discovery_json['Data']['MainboardID']
+
+        self._connection = None
+        self._is_connected = False
+
         self._status = None
 
     @property
@@ -28,25 +34,84 @@ class SDCPPrinter:
     def mainboard_id(self) -> str:
         return self._mainboard_id
 
+    @property
+    def _websocket_url(self) -> str:
+        return f'ws://{self._ip_address}:{PRINTER_PORT}/websocket'
+
+    def start_listening(self) -> None:
+        '''Opens a persistent connection to the printer to listen for messages.'''
+        self._connection = websocket.WebSocketApp(
+            self._websocket_url,
+            on_open=self._on_open,
+            on_close=self._on_close,
+            on_message=self._on_message,
+        )
+        threading.Thread(target=self._connection.run_forever).start()
+        while not self._is_connected:
+            time.sleep(0.1)
+
+    def stop_listening(self) -> None:
+        '''Closes the connection to the printer.'''
+        self._connection.close()
+
+    def _on_open(self, ws) -> None:
+        logger.info(f'{self._ip_address}: Connection opened')
+        self._is_connected = True
+
+    def _on_close(self, ws, close_status_code, close_msg) -> None:
+        logger.info(f'{self._ip_address}: Connection closed')
+        self._is_connected = False
+
+    def _on_message(self, ws, message: str) -> SDCPMessage:
+        logger.debug(f'{self._ip_address}: Message received: {message}')
+        parsed_message = SDCPMessage.parse(message)
+
+        match parsed_message.topic:
+            case 'response':
+                pass
+            case 'status':
+                self._update_status(parsed_message)
+            case _:
+                logger.warning(f'{self._ip_address}: Unknown message topic')
+
+        return parsed_message
+
     def refresh_status(self) -> None:
         '''Sends a request to the printer to report its status.'''
         logger.info(f'{self._ip_address}: Requesting status')
 
         payload = SDCPStatusRequest.build(self)
 
-        status_message: SDCPStatusMessage = self.send_request(payload)
-        self._status = status_message.status
+        self._send_request(payload)
 
-    def send_request(self, payload: dict, expect_response: bool = True) -> SDCPMessage:
+    def _send_request(
+            self,
+            payload: dict,
+            connection: websocket = None,
+            receive_message: bool = True,
+            expect_response: bool = True
+    ) -> SDCPMessage:
         '''Sends a request to the printer.'''
+        if connection is None:
+            if self._connection is not None and self._is_connected:
+                return self._send_request(payload, self._connection, receive_message=False)
+            else:
+                with closing(websocket.create_connection(self._websocket_url)) as ws:
+                    return self._send_request(payload, ws, receive_message=True, expect_response=expect_response)
+
         logger.debug(
             f'{self._ip_address}: Sending request with payload: {payload}')
-        with closing(websocket.create_connection(f'ws://{self._ip_address}:{PRINTER_PORT}/websocket')) as ws:
-            ws.send(json.dumps(payload))
+        connection.send(json.dumps(payload))
 
+        if receive_message:
             if expect_response:
-                response: SDCPResponseMessage = SDCPMessage.parse(ws.recv())
+                response: SDCPResponseMessage = self._on_message(
+                    connection.recv())
                 if not response.is_success():
                     raise Exception('Request failed')
+            return self._on_message(connection.recv())
 
-            return SDCPMessage.parse(ws.recv())
+    def _update_status(self, message: SDCPStatusMessage) -> None:
+        '''Updates the printer's status fields.'''
+        self._status = message.status
+        logger.info(f'{self._ip_address}: Status updated: {self._status}')
